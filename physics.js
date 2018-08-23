b4e04e5673dcec1a903ebdcc0303b297a942148d
EmilSchroder <eschro66@gmail.com>
2018-08-23 16:00:10 +1200

    left and right

--- physics.js
+++ physics.js
@@ -11,6 +11,41 @@ let ballVel = 0;
 let vel;
 let up = false;
 
+var keymap ={
+    "a": false,
+    "d": false,
+    "s": false,
+    "w": false
+};
+
+//Listen for button pushes
+document.addEventListener("keydown",keyHandler, false);
+document.addEventListener("keyup", keyHandler, false);
+
+//What happens when key pushed or released
+function keyHandler(evt){
+    keymap[evt.key] = (evt.type == 'keydown');
+}
+
+
+//Movement parameters
+function isLeft(){
+    if (keymap["a"] && x - ballRad > 0){
+        x -= 2;
+    }
+}
+
+function isRight(){
+    if (keymap["d"] && x + ballRad < canvas.width){
+        x += 2;
+    }
+}
+
+function isReversed(grav){
+    if (keymap["s"]){
+        grav = -grav;
+    }
+}
 
 function drawBall(){
     ctx.beginPath();
@@ -25,7 +60,7 @@ function applyGravity(){
     let endTime = new Date();
     let grav = 5;
     let timeDelta = (endTime - startTime)/1000;
-
+    isReversed(grav)
     if(up){
         vel = 0.99*vel;
         dy =  -vel + 0.5*grav*timeDelta*timeDelta;
@@ -36,7 +71,6 @@ function applyGravity(){
     } else {
         dy =  0.5*grav*timeDelta*timeDelta;
         vel = dy/timeDelta;
-        maxvel = vel;
         // vel = (Math.sqrt(2*grav*(y-inity)));
     }
     
@@ -51,6 +85,14 @@ function bottomCollision(){
     }
 }
 
+function topCollision(){
+    if (y < (ballRad*2) + 100){
+        up = false;
+        startTime = new Date();
+        
+    }
+}
+
 function bounceHeight(){
     if(y >= canvas.height){
         up = false;
@@ -59,6 +101,9 @@ function bounceHeight(){
 }
 
 function moveBall(){
+
+    isLeft()
+    isRight()
     y = y + dy;
 }
 
@@ -67,6 +112,7 @@ function draw(){
 
     applyGravity();
     bottomCollision();
+    topCollision();
     moveBall();
     drawBall();
     
